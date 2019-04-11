bdda1ecfe49fef0d04e10e38276588279dc589f0
mp0liiu <raian@reeshome.org>
2019-04-11 23:19:04 +0900

    hear test

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 module.exports = (robot) => {
+    robot.hear(/aa/, (msg) => {
+        msg.send('A', 'B');
+    });
     robot.respond(/say-hw/, (msg) => {
         msg.reply('Hello, World');
         msg.reply('What is this...');
