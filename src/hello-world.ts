74246bfd3ddd28e03f4a17805a02c3e95fe62ae1
mp0liiu <raian@reeshome.org>
2019-04-16 04:35:40 +0900

    try to use eslint but...

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -4,7 +4,7 @@ module.exports = (robot: Robot<any>): void => {
 
     robot.hear(/aa/, (msg: Response<Robot<any>>) => {
         msg.send('A', 'B');
-    })
+    });
 
     robot.respond(/say-hw/, (msg: Response<Robot<any>>) => {
         msg.reply('Hello, World');
--- src/practice.ts
+++ src/practice.ts
@@ -1,7 +1,7 @@
 import { Robot, Response } from 'hubot';
 
 module.exports = (robot: Robot<any>): void => {
-  
+
   robot.hear(/hey!/, (res: Response<Robot<any>>) => {
     res.send('hello!');
   });
